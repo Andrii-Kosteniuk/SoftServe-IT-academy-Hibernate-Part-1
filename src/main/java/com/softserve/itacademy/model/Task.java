8d18c42c28a5215e9a4aabe6281dbdb917ab9906
Yurii Komarnytskyi <ykomarnytskyi2022@gmail.com>
2024-11-14 19:29:22 +0100

    Created entity Task

--- src/main/java/com/softserve/itacademy/model/Task.java
+++ src/main/java/com/softserve/itacademy/model/Task.java
@@ -1,16 +1,88 @@
 package com.softserve.itacademy.model;
 
-import jakarta.persistence.*;
-import java.util.Objects;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
+@Entity
 public class Task {
-
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+	
     private String name;
+    
+    @Enumerated(EnumType.STRING)
     private TaskPriority priority;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "todo_id")
     private ToDo todo;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "state_id")
     private State state;
+    
+	public Task() { }
+	
+	public Task(long id, String name, TaskPriority priority, ToDo todo, State state) {
+		this.id = id;
+		this.name = name;
+		this.priority = priority;
+		this.todo = todo;
+		this.state = state;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public TaskPriority getPriority() {
+		return priority;
+	}
+
+	public void setPriority(TaskPriority priority) {
+		this.priority = priority;
+	}
+
+	public ToDo getTodo() {
+		return todo;
+	}
+
+	public void setTodo(ToDo todo) {
+		this.todo = todo;
+		todo.getTasks().add(this);
+	}
+
+	public State getState() {
+		return state;
+	}
+
+	public void setState(State state) {
+		this.state = state;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	@Override
+	public String toString() {
+		return "Task [id=" + id + ", name=" + name + ", priority=" + priority + ", todo=" + todo + ", state=" + state
+				+ "]";
+	}
+	
+	
 
 }
